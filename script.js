c77ce161e37c4f6f49cd4832ea112ed8f2a0dae3
Aarya Paradkar <119689819+aaryaparadkar@users.noreply.github.com>
2023-08-31 09:25:15 +0530

    updated

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 // typed js
 const typed = new Typed('.multiple-text', {
-    strings: ['Student', 'Frontend Developer', 'Cyber Security Enthusiast'],
+    strings: ['Fullstack Developer', 'Blockchain Novice', 'Cyber Security Enthusiast'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
